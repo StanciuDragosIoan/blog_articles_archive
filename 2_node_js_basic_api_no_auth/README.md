a77f4d4494d944188faced8be011d1cf3b16910a
stanciudragosioan <stanciudragosioan92@gmail.com>
2020-09-06 23:47:48 +0300

    initial commit

---
+++ 2_node_js_basic_api_no_auth/README.md
@@ -0,0 +1,5 @@
+This is a basic node JS API with full crud functionality
+
+The howTo.html is an article showing how to build the project from scratch.
+
+This is one of the articles for my blog.
---
+++ notes.js
@@ -0,0 +1,12 @@
+/*
+Articles outline:
+
+1. web dev introduction - diary/journal APP
+
+2. basic CRUD API nodeJS
+
+3. basic CRUD API with basic auth (No relations, all users see same resources)
+
+4. CRUD Api with token auth and relations (each user sees only his resources)
+
+*/
